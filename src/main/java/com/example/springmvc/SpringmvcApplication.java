b1ffa9cc5546e0191ed623a18d5a7f215201a3ca
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-12-30 16:01:44 +0100

    configured railway deployment and readjusted some app styling

--- src/main/java/com/example/springmvc/SpringmvcApplication.java
+++ src/main/java/com/example/springmvc/SpringmvcApplication.java
@@ -1,11 +1,20 @@
 package com.example.springmvc;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.springmvc.dao.PatientRepository;
+import com.example.springmvc.entities.Patient;
+
 @SpringBootApplication
 public class SpringmvcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcApplication.class, args);
@@ -13,8 +22,13 @@ public class SpringmvcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // TODO Auto-generated method stub
-
+//        patientRepository.save(new Patient(null, " Imaizumi", new Date(), false, 4));
+//        patientRepository.save(new Patient(null, " Manami", new Date(), false, 5));
+//        patientRepository.save(new Patient(null, " Naruko", new Date(), false, 4));
+//        
+//        patientRepository.findAll().forEach(p -> {
+//        	System.out.println(p.getName());
+//        });
     }
 
 }
